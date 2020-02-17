616b9fc5f36d9b76aac8603f36487e3aec5f594f
Ubuntu <ubuntu@ip-172-31-82-145.ec2.internal>
2020-02-17 02:42:03 +0000

    comment

--- HelloWorld.swift
+++ HelloWorld.swift
@@ -1,1 +1,10 @@
+// 
+//  HelloWorld.swift
+//  ICS4U-Unit1-01-Swift
+// 
+//  Created by Christina on 10/02/20.
+//  Copyright © Christina Ngwa. All rights reserved.
+// 
+// This program says hello world!
+// 
 print("Hello, World!")
